8bd8c676c3b54beb35ef5c9ccd0aa11499e12efb
Andres Palma <palma.castaneda030@gmail.com>
2021-08-31 22:49:00 -0500

    cambio total

--- script/displayCarrito.js
+++
@@ -1,9 +0,0 @@
-export {displayCarrito}
-
-const section = document.querySelector('section')
-const contenedorCarrito = document.querySelector('#contenedor-carrito')
-
-const displayCarrito = e => {
-    section.classList.toggle('displayNone')
-    contenedorCarrito.classList.toggle('displayNone')
-}
--- script/index.js
+++
@@ -1,3 +0,0 @@
-import { getProductos } from "./getProductos.js";
-
-getProductos(0)
